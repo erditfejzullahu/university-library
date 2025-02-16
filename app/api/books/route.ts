8db2ea9f55dec80b1215e66a714dbed289416e08
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-02-16 23:42:40 +0100

    book creation api added

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -16,3 +16,21 @@ export async function GET(){
         return NextResponse.json({message: "Error in retriving books"}, {status: 404})
     }
 }
+
+export async function POST(res: Request) {
+    const bookData = await res.json();
+    try {
+        if(!bookData){
+            return NextResponse.json({message: "Missing data"}, {status: 400})
+        }
+
+        const newBook = await prisma.books.create({
+            data: bookData
+        })
+
+        return NextResponse.json({newBook, message: "Book added successfully"}, {status:200});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message: "Error in adding book"}, {status:400})
+    }
+}
